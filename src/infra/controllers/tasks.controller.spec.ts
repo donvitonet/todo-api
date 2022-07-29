6f056640ce3e28ac7fa235e88203db55ec45c067
Reginaldo Lima <donvitonet@gmail.com>
2022-07-29 18:00:00 -0300

    test: add unit test to create task.

--- src/infra/controllers/tasks.controller.spec.ts
+++ src/infra/controllers/tasks.controller.spec.ts
@@ -1,18 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Task, UniqueEntityID } from '@entities';
+import { Tasks } from '@useCases';
 import { TasksController } from './tasks.controller';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let completeTaskInteractor: Tasks.CompleteTask.CompleteTaskInteractor;
+  let createTaskInteractor: Tasks.CreateTask.CreateTaskInteractor;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TasksController],
-    }).compile();
+    completeTaskInteractor = new Tasks.CompleteTask.CompleteTaskInteractor(
+      null,
+    );
+    createTaskInteractor = new Tasks.CreateTask.CreateTaskInteractor(null);
 
-    controller = module.get<TasksController>(TasksController);
+    controller = new TasksController(
+      completeTaskInteractor,
+      createTaskInteractor,
+      null,
+      null,
+      null,
+      null,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return the task created', async () => {
+    const task = Task.build(
+      {
+        name: 'the task created',
+      },
+      new UniqueEntityID(1),
+    ).value;
+
+    jest
+      .spyOn(createTaskInteractor, 'execute')
+      .mockImplementation(() => Promise.resolve(task));
+
+    expect(
+      await controller.create({
+        name: 'the task created',
+      }),
+    ).toEqual({
+      data: {
+        id: 1,
+        name: 'the task created',
+        done: false,
+      },
+    });
   });
 });
